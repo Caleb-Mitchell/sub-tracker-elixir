0d1dc1b2527e40d8baaa51c0800395a778cbed0e
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-07-11 15:38:56 -0500

    Add pagination logic to controller

--- lib/sub_tracker_elixir_web/controllers/instrument_controller.ex
+++ lib/sub_tracker_elixir_web/controllers/instrument_controller.ex
@@ -1,37 +1,27 @@
 defmodule SubTrackerElixirWeb.InstrumentController do
   use SubTrackerElixirWeb, :controller
 
-  import Ecto.Query
+  alias SubTrackerElixir.Instrument
 
   def instruments(conn, _params) do
-    query =
-      from i in "instruments",
-        distinct: true,
-        left_join: m in "musicians",
-        on: i.id == m.instrument_id,
-        order_by: i.name,
-        select: {i.name, i.id}
+    instrument_list = Instrument.list_instruments()
+    instrument_count = Instrument.find_item_count(instrument_list)
 
-    instrument_list =
-      SubTrackerElixir.Repo.all(query)
-      |> Enum.map(fn {name, id} -> %{name: name, id: id} end)
-
-    instrument_count = length(instrument_list)
+    page_num = Instrument.load_page()
+    last_page = Instrument.find_last_page_number(instrument_count)
+    min_page = Instrument.find_pagination_min_page(page_num, last_page)
+    max_page = Instrument.find_pagination_max_page(page_num, last_page)
 
     render(conn,
       page_title: "Instrument List",
       instrument_list: instrument_list,
-      instrumnet_count: instrument_count
+      instrument_count: instrument_count,
+      min_page: min_page,
+      max_page: max_page,
+      page_num: page_num,
+      last_page: last_page
     )
   end
 
-  # here is what i need to have access to via `assigns` to display
-  # the rest of my dynamic html for my instruments page
-
-  # @instrument_count = @storage.total_instrument_count
-  # @last_page = find_last_page_number(@instrument_count)
-  # @page_num = load_page(params[:page], @last_page) || 1
-  # @min_page, @max_page = find_min_and_max_page_num(@last_page,
-  #                                                  @instrument_count)
   # @instruments_page_list = @storage.instruments_page(@page_num)
 end
