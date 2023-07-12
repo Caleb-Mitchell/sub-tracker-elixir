687311dac817c28879e1ee799aca4611f84d615e
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-07-12 16:38:37 -0500

    Finish view and logic for instrument list and pagination

--- lib/sub_tracker_elixir/instrument.ex
+++ lib/sub_tracker_elixir/instrument.ex
@@ -14,11 +14,20 @@ defmodule SubTrackerElixir.Instrument do
   def list_instruments do
     query =
       from i in "instruments",
-        distinct: true,
-        left_join: m in "musicians",
-        on: i.id == m.instrument_id,
         order_by: i.name,
         select: {i.name, i.id}
+
+    SubTrackerElixir.Repo.all(query)
+    |> Enum.map(fn {name, id} -> %{name: name, id: id} end)
+  end
+
+  def instruments_page(page_num) do
+    query =
+      from i in "instruments",
+        order_by: i.name,
+        select: {i.name, i.id},
+        limit: @items_per_page,
+        offset: (@items_per_page * (^page_num - 1))
 
     SubTrackerElixir.Repo.all(query)
     |> Enum.map(fn {name, id} -> %{name: name, id: id} end)
@@ -28,33 +37,67 @@ defmodule SubTrackerElixir.Instrument do
     length(instrument_list)
   end
 
-  def load_page do
-    # TODO:flesh out logic for other pages soon
-    1
+  def load_page(page_num) do
+    # TODO:add error validation later
+    if page_num == nil do
+      1
+    else
+      String.to_integer(page_num)
+    end
   end
 
-  def find_pagination_min_page(current_page, last_page) do
+  def pagination_necessary?(item_count) do
+    item_count > @items_per_page
+  end
+
+  def first_page?(current_page) do
+    current_page == 1
+  end
+
+  def last_page?(current_page, last_page) do
+    current_page == last_page
+  end
+
+  def find_current_visible_page_range(item_count, current_page) do
+    chunked = Enum.chunk_every(1..item_count, 5, 1, :discard)
+
+    Enum.filter(chunked, fn subarr -> Enum.at(subarr, div(length(subarr), 2)) == current_page end)
+    |> List.flatten()
+  end
+
+  def find_pagination_slice_min(item_count, current_page) do
+    page_range = find_current_visible_page_range(item_count, current_page)
+    Enum.min(page_range)
+  end
+
+  def find_pagination_slice_max(item_count, current_page) do
+    page_range = find_current_visible_page_range(item_count, current_page)
+    Enum.max(page_range)
+  end
+
+  def find_pagination_min_page(current_page, last_page, item_count) do
     cond do
       current_page <= div(@num_pages_visible, 2) ->
-        # TODO: does current page need to be converted to a string
         1
 
       current_page >= last_page - div(@num_pages_visible, 2) ->
         max(last_page - @num_pages_visible + 1, 1)
-        # true ->
-        #   find_pagination_low_slice(item_count)
+
+      true ->
+        find_pagination_slice_min(item_count, current_page)
     end
   end
 
-  def find_pagination_max_page(current_page, last_page) do
+  def find_pagination_max_page(current_page, last_page, item_count) do
     cond do
       current_page <= div(@num_pages_visible, 2) ->
         min(last_page, @num_pages_visible)
 
       current_page >= last_page - div(@num_pages_visible, 2) ->
         last_page
-        # true ->
-        #   find_pagination_low_slice(item_count)
+
+      true ->
+        find_pagination_slice_max(item_count, current_page)
     end
   end
 
@@ -62,7 +105,7 @@ defmodule SubTrackerElixir.Instrument do
     if rem(item_count, @num_pages_visible) == 0 do
       div(item_count, @num_pages_visible)
     else
-      div(item_count, @num_pages_visible + 1)
+      div(item_count, @num_pages_visible) + 1
     end
   end
 end
--- lib/sub_tracker_elixir_web/controllers/instrument_controller.ex
+++ lib/sub_tracker_elixir_web/controllers/instrument_controller.ex
@@ -7,14 +7,17 @@ defmodule SubTrackerElixirWeb.InstrumentController do
     instrument_list = Instrument.list_instruments()
     instrument_count = Instrument.find_item_count(instrument_list)
 
-    page_num = Instrument.load_page()
+    page_num = Instrument.load_page(conn.params["page"])
+    instrument_page_list = Instrument.instruments_page(page_num)
+
     last_page = Instrument.find_last_page_number(instrument_count)
-    min_page = Instrument.find_pagination_min_page(page_num, last_page)
-    max_page = Instrument.find_pagination_max_page(page_num, last_page)
+    min_page = Instrument.find_pagination_min_page(page_num, last_page, instrument_count)
+    max_page = Instrument.find_pagination_max_page(page_num, last_page, instrument_count)
 
     render(conn,
       page_title: "Instrument List",
       instrument_list: instrument_list,
+      instrument_page_list: instrument_page_list,
       instrument_count: instrument_count,
       min_page: min_page,
       max_page: max_page,
@@ -22,6 +25,4 @@ defmodule SubTrackerElixirWeb.InstrumentController do
       last_page: last_page
     )
   end
-
-  # @instruments_page_list = @storage.instruments_page(@page_num)
 end
