acd441272cbf80be7fac9f77e8a6bf5985510d50
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-07-17 16:29:36 -0500

    Force ssl in config

--- config/runtime.exs
+++ config/runtime.exs
@@ -90,8 +90,7 @@ if config_env() == :prod do
   # We also recommend setting `force_ssl` in your endpoint, ensuring
   # no data is ever sent via http, always redirecting to https:
   #
-  #     config :sub_tracker_elixir, SubTrackerElixirWeb.Endpoint,
-  #       force_ssl: [hsts: true]
+  config :sub_tracker_elixir, SubTrackerElixirWeb.Endpoint, force_ssl: [hsts: true]
   #
   # Check `Plug.SSL` for all available options in `force_ssl`.
 
